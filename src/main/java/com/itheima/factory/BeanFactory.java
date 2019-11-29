c679887a92918e2808d7486b322c478b4c99a1ad
waking777 <756274132@qq.com>
2019-11-29 10:47:29 +0800

    one day fish and thirty days rest

--- src/main/java/com/itheima/factory/BeanFactory.java
+++ src/main/java/com/itheima/factory/BeanFactory.java
@@ -1,6 +1,9 @@
 package com.itheima.factory;
 
 import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -17,6 +20,9 @@ public class BeanFactory {
     //定义一个properties
     private static Properties props;
 
+    //定义一个Map,来存放我们需要创建的对象。我们吧他称之为容器
+    private static Map<String,Object> beans;
+
     //使用静态代码块为properties对象赋值
     static {
         try {
@@ -25,27 +31,51 @@ public class BeanFactory {
             //获取properties文件的流对象
             InputStream in = BeanFactory.class.getClassLoader().getResourceAsStream("bean.properties");
             props.load(in);
+            //实例化容器
+            beans = new HashMap<String, Object>();
+            //取出配置文件中所有的key
+            Enumeration keys =  props.keys();
+            //遍历枚举
+            while (keys.hasMoreElements()){
+                //取出每个key
+                String key = keys.nextElement().toString();
+                //根据key来获取value
+                String beanPath = props.getProperty(key);
+                //反射创建对象
+                Object value = Class.forName(beanPath).newInstance();
+                //把key和value存入容器之中
+                beans.put(key,value);
+            }
         } catch (Exception e) {
             throw new ExceptionInInitializerError("初始化properties失败");
         }
     }
 
     /**
-     * 根据bean的名字来获取bean对象
-     *
+     * 根据bean的名称获取对象--单例
      * @param beanName
      * @return
      */
     public static Object getBean(String beanName) {
+        return beans.get(beanName);
+    }
+
+    /**
+     * 根据bean的名字来获取bean对象--多例
+     *
+     * @param beanName
+     * @return
+
+    public static Object getBean(String beanName) {
         Object bean = null;
         try {
             String beanpath = props.getProperty(beanName);
-            System.out.println(beanpath);
-            bean = Class.forName(beanpath).newInstance();
+            bean = Class.forName(beanpath).newInstance();//每次都会用默认构造函数来创建对象
         } catch (Exception e) {
             e.printStackTrace();
         }
         return bean;
-    }
+     */
+
 
 }
--- src/main/java/com/itheima/ui/Client.java
+++ src/main/java/com/itheima/ui/Client.java
@@ -10,7 +10,10 @@ import com.itheima.service.IAccountService;
 public class Client {
     public static void main(String[] args) {
 //        IAccountService as = new AccountServiceImpl();
-        IAccountService as = (IAccountService) BeanFactory.getBean("accountService");
-        as.saveAccount();
+        for(int i=0 ;i<5;i++ ) {
+            IAccountService as = (IAccountService) BeanFactory.getBean("accountService");
+            System.out.println(as);
+        }
+//        as.saveAccount();
     }
 }
