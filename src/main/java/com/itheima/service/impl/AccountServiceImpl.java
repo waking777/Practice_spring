1f0cfce4820613bbcc3f57774aff9133b1979bfd
waking777 <756274132@qq.com>
2019-12-02 13:47:52 +0800

    one day fish and thirty days rest

--- src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -5,14 +5,28 @@ import com.itheima.dao.impl.AccountDaoImpl;
 import com.itheima.factory.BeanFactory;
 import com.itheima.service.IAccountService;
 
+import java.util.Date;
+
 /**
  * 账户的业务层实现类
  */
 public class AccountServiceImpl implements IAccountService {
 
-    private IAccountDao accountDao = new AccountDaoImpl();
-//    private IAccountDao accountDao = (IAccountDao) BeanFactory.getBean("accountDao");
-    public void saveAccount() {
-        accountDao.saveAccount();
+    private String name;
+    private Integer age;
+    private Date birthday;
+
+    public AccountServiceImpl(String name, Integer age, Date birthday) {
+        this.age = age;
+        this.name = name;
+        this.birthday = birthday;
     }
+//    private IAccountDao accountDao = new AccountDaoImpl();
+
+    //    private IAccountDao accountDao = (IAccountDao) BeanFactory.getBean("accountDao");
+    public void saveAccount() {
+        System.out.println("service中的saveAccount的方法执行了" + name + age + birthday);
+    }
+
+
 }
--- src/main/java/com/itheima/ui/Client.java
+++ src/main/java/com/itheima/ui/Client.java
@@ -16,17 +16,18 @@ public class Client {
 
     /**
      * 获取spring的Ioc核心容器，并根据id来获取对象
-     *
-     **ApplicationContext的三个常用实现类
-     *     ClassPathXmlApplicationContext      :他可以加载类路径下的配置文件，要求配置文件必须在类路径下,不在的加载不了
-     *     FileSystemXmlApplicationContext     :他可以加载磁盘任意路径下的配置文件，必须有访问权限。
-     *     AnnotationConfigApplicationContext  :他是用于读取注解创建容器的。
-     *
+     * <p>
+     * *ApplicationContext的三个常用实现类
+     * ClassPathXmlApplicationContext      :他可以加载类路径下的配置文件，要求配置文件必须在类路径下,不在的加载不了
+     * FileSystemXmlApplicationContext     :他可以加载磁盘任意路径下的配置文件，必须有访问权限。
+     * AnnotationConfigApplicationContext  :他是用于读取注解创建容器的。
+     * <p>
      * 核心容器的两个接口引发出的问题：
      * ApplicationContext：
-     *      他在构建核心容器时，创建对象采用的策略是采用即使加载的方式。也就是说，一读取完配置文件就马上创建文件中的对象
+     * 他在构建核心容器时，创建对象采用的策略是采用即使加载的方式。也就是说，一读取完配置文件就马上创建文件中的对象
      * BeanFactory:
-     *      他在构建核心容器时，创建对象采用的策略是采用的延迟加载的方式。什么时候用，什么时候创建。
+     * 他在构建核心容器时，创建对象采用的策略是采用的延迟加载的方式。什么时候用，什么时候创建。
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -36,15 +37,16 @@ public class Client {
 //            System.out.println(as);
 //        }
         //1.获取核心容器对象
-//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");  //常用
-        ApplicationContext ac = new FileSystemXmlApplicationContext("D:\\develop\\idea\\IdeaProjects\\xinjian" +
-                "\\Practice_spring\\src\\main\\resources\\bean.xml");
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");  //常用
+//        ApplicationContext ac = new FileSystemXmlApplicationContext("D:\\develop\\idea\\IdeaProjects\\xinjian" +
+//                "\\Practice_spring\\src\\main\\resources\\bean.xml");
         //2.根据ID来获取bean对象
         IAccountService as = (IAccountService) ac.getBean("accountService");
-        IAccountDao adao = ac.getBean("accountDao",IAccountDao.class);
+        IAccountDao adao = ac.getBean("accountDao", IAccountDao.class);
 
-        System.out.println(as);
-        System.out.println(adao);
-//        as.saveAccount();
+        as.saveAccount();
+
+        //手动关闭容器
+        ac.close();
     }
 }
